7e0c36ed994c7025c3b1cda79a8ae0d05f0da531
Eliad Degabli <eliadegabli@gmail.com>
2023-04-21 15:55:08 +0300

    check env properties

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", function(req,res){
-    res.send("it worked")
+    res.send("it worked" + process.env.REACT_APP_HOST_AWS_SQL)
 })
 
 app.listen(process.env.PORT || 5000)
